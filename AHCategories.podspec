bd6b0a248c3263839cc7cabf0eeea7b3ac2f54a5
黄辉 <huanghui.@meizu.com>
2016-09-19 12:48:39 +0800

    Fix to swift 3.0

--- AHCategories.podspec
+++ AHCategories.podspec
@@ -1,11 +1,11 @@
 Pod::Spec.new do |s|
   s.name         = "AHCategories"
-  s.version      = "1.0.0"
+  s.version      = "1.0.1"
   s.summary      = "工具类"
   s.homepage     = "https://github.com/AnsonHui/AHCategories"
   s.license      = "MIT"
   s.author             = { "黄辉" => "fantasyhui@126.com" }
-  s.source       = { :git => "https://github.com/AnsonHui/AHCategories.git", :tag => "s.version" }
+  s.source       = { :git => "https://github.com/AnsonHui/AHCategories.git", :tag => s.version }
   s.source_files  = "Classes/*.swift"
   s.requires_arc = true
   s.ios.deployment_target = "8.0"
--- AHCategories/ViewController.swift
+++ AHCategories/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  AHCategories
 //
-//  Created by 黄辉 on 8/5/16.
-//  Copyright © 2016 Fantasy. All rights reserved.
+//  Created by 黄辉 on 9/18/16.
+//  Copyright © 2016 AnsonHui. All rights reserved.
 //
 
 import UIKit
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
 
         // 颜色
-        let label = UILabel(frame: CGRectMake(40, 40, 200, 30))
+        let label = UILabel(frame: CGRect(x: 40, y: 40, width: 200, height: 30))
         label.backgroundColor = UIColor(argb: 0xff343434)
         label.textColor = UIColor(argb: 0xffababab)
         label.text = "测试颜色"
